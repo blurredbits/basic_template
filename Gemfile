c38121ab36c61f57b27fd7067864f15e51781b66
Blurredbits <mmorris00@mac.com>
2013-10-27 12:06:53 -0600

    Initial commit

---
+++ Gemfile
@@ -0,0 +1,24 @@
+source 'https://rubygems.org'
+
+# rails, etc
+gem 'rails', '4.0.0'
+gem 'sass-rails', '~> 4.0.0'
+gem 'coffee-rails', '~> 4.0.0'
+
+# drivers
+gem 'pg'
+
+# other
+gem 'uglifier', '>= 1.3.0'
+
+# view gems
+gem 'jquery-rails'
+gem 'jbuilder', '~> 1.2'
+gem 'bootstrap-sass-rails'
+gem 'haml-rails'
+
+group :development, :test do
+  gem 'rspec-rails'
+  gem 'rspec-nc'
+end
+
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BasicTemplate::Application.config.secret_key_base = 'f1926e0b1b5deb12ee92191f494f0d9337c49660aee666ac81b55693386021268b93b5309d009d77e18c058767b4b02f673c4461a2426cbe2c1d36ebe932c317'
